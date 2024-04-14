325924ff8162b75edc024a1503a627c627d799f2
Keith986 <keithkelly986@gmail.com>
2024-04-14 14:30:09 +0300

    committ

--- public/style.js
+++ public/style.js
@@ -7,11 +7,8 @@ $(document).ready(function(){
     })
 })
 
-    //toggle description
-    $(document).ready(function(){
-        $('#dot').on('click', function(){
-        $('.description').animate({
-         show: 'toggle'
-          })
-          })
-          })
+
+
+
+     
+   
